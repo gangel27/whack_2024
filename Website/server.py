b3fe1f2e7448329428f9e17fa505ab071d1128c9
gangel27 <georgeangeluk@gmail.com>
2024-10-26 11:58:58 +0100

    added template flask file

---
+++ Website/server.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request
+import json
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/data", methods=["GET"])
+def returnData():
+    with open("data.txt") as file:
+        data = file.read()
+    print(data)
+    return data, 200
+
+
+@app.route("/post", methods=["POST"])
+def get_post_javascript_data():
+    jsdata = request.form.to_dict(flat=False)
+    print(jsdata)
+    with open("data.txt", "w") as file:
+        file.truncate(0)
+        file.write(jsdata["data"][0])
+
+    return jsdata, 200
+
+
+@app.route("/json", methods=["GET"])
+def sendjson():
+    with open("data.json", "r") as j:
+        contents = json.loads(j.read())
+        print(contents["data"])
+    return contents, 200
+
+
+if __name__ == "__main__":
+    app.run()
